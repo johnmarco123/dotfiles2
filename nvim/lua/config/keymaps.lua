bc396cd88c2dbfe252ba951b22a47e3e88164208
John-Marco <johndoe@example.com>
2024-10-03 09:34:45 -0400

    autosaved from save script...

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -13,7 +13,6 @@ vim.keymap.set("n", "<leader>td", ":edit ~/vimwiki/Tasks.wiki <CR>")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 -- theprimeagen neov
 vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "Y", "yg$")
