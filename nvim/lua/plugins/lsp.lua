cba9e7be0044c7fe16e8567d5a1795e6838420f0
johnmarco123 <johnmarcotasillo@gmail.com>
2024-10-06 11:26:25 -0400

    autosaved from save script...

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -52,7 +52,7 @@ return {
             },
             mapping = cmp.mapping.preset.insert({
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping.confirm({select = true}),
                 ['<C-Space>'] = cmp.mapping.complete(),
             }),
