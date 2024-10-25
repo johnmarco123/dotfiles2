024a8db519e95f0a39351c9fe1b1d862419f75f4
John-Marco <johndoe@example.com>
2024-10-25 08:55:29 -0400

    autosaved from save script...

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,8 +4,10 @@ return {
     config = function()
         vim.cmd('colorscheme tokyonight-night');
         vim.cmd([[autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({higroup="IncSearch", timeout=50})]]);
-        vim.cmd [[ highlight IncSearch guibg=#FF8C00]] -- changes the yank color
-        vim.cmd('highlight LineNr guifg=#00FFFF')  -- Change the color to your preference
+        vim.api.nvim_set_hl(0, 'LineNrAbove', { fg='#51B3EC', bold=true })
+        vim.api.nvim_set_hl(0, 'LineNr', { fg='white', bold=true })
+        vim.api.nvim_set_hl(0, 'LineNrBelow', { fg='#FB508F', bold=true })
+        vim.api.nvim_set_hl(0, 'IncSearch', { bg='#FF8C00', bold=true })
     end
     }
     --{
